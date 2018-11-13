7f5e59bf1f8097ad2b189099851ab110506abf96
Khanh Tran <32532742+khanhtc1202@users.noreply.github.com>
2018-11-13 14:21:51 +0900

    Update net_example_test.go

--- net_example_test.go
+++ net_example_test.go
@@ -11,7 +11,7 @@ func ExampleDialFunc() {
 	resolver, _ := New(3*time.Second, 5*time.Second, zap.NewNop())
 
 	// You can create a HTTP client which selects an IP from dnscache
-	// ramdomly and dial it.
+	// randomly and dial it.
 	client := http.Client{
 		Transport: &http.Transport{
 			DialContext: DialFunc(resolver, nil),
